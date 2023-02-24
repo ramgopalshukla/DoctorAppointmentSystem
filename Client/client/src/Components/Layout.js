2042fab9147550547bf7159500bda4fbebd9137f
ramgopalshukla <gopalshukla28499@gmail.com>
2023-02-24 18:29:49 +0530

    applu doctor backend done

--- Client/client/src/Components/Layout.js
+++ Client/client/src/Components/Layout.js
@@ -3,7 +3,7 @@ import '../Pages/styles/Layoutstyles.css'
 import { userMenu, adminMenu } from '../Data/data'
 import {Link, useLocation, useNavigate }from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { message } from 'antd'
+import { message, Badge } from 'antd'
 export default function Layout({children}) {
  const navigate= useNavigate();
   const location= useLocation();
@@ -16,7 +16,7 @@ export default function Layout({children}) {
 const handleLogout= ()=>{
   localStorage.clear()
   message.success("Logout Succesfully");
-  navigate('./login')
+  navigate('/login')
 }
   //  rendoring menu list
 
@@ -52,7 +52,10 @@ const handleLogout= ()=>{
             <div className="content">
                 <div className="header">
                   <div className="header-content">
-                    <i className='fa-solid fa-bell'></i>
+                  <Badge count={user&& user.notification.length}>
+                  <i className='fa-solid fa-bell'></i>
+                     </Badge>
+                   
                     <Link to="/profile">{user?.name}</Link>
                   </div>
                 </div>
--- Client/client/src/Pages/ApplyDoctor.js
+++ Client/client/src/Pages/ApplyDoctor.js
@@ -1,10 +1,44 @@
 import React from "react";
 import Layout from "../Components/Layout";
-import { Form, Row, Col, Input, TimePicker, RangePicker } from "antd";
+import { Form, Row, Col, Input, TimePicker, RangePicker, message } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { showLoading, hideLoading } from "./Redux/features/alertSlice";
+import axios from "axios";
 
 export default function ApplyDoctor() {
-  const handleFinish = (values) => {
-    console.log(values);
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => {
+    return state.user;
+  });
+  const handleFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/apply-doctor",
+        { ...values, userId: user._id },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem(`token`)}`,
+          },
+        }
+      );
+
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.success);
+        navigate('/');
+      }else{
+        message.error(res.data.success)
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("Something Went Wrong");
+    }
   };
   return (
     <Layout>
@@ -15,7 +49,7 @@ export default function ApplyDoctor() {
           <Col xs={24} md={24} lg={8}>
             <Form.Item
               label="First Name"
-              name="firstname"
+              name="firstName"
               required
               rules={[{ required: true }]}
             >
@@ -25,7 +59,7 @@ export default function ApplyDoctor() {
           <Col xs={24} md={24} lg={8}>
             <Form.Item
               label="Last Name"
-              name="lastname"
+              name="lastName"
               required
               rules={[{ required: true }]}
             >
@@ -49,27 +83,27 @@ export default function ApplyDoctor() {
               required
               rules={[{ required: true }]}
             >
-              <Input type="text" placeholder="your email adsress" />
+              <Input type="text" placeholder="your email address" />
             </Form.Item>
           </Col>
           <Col xs={24} md={24} lg={8}>
             <Form.Item
               label="Website"
-              name="your website"
+              name="website"
               required
               rules={[{ required: true }]}
             >
-              <Input type="text" placeholder="your first name" />
+              <Input type="text" placeholder="your website" />
             </Form.Item>
           </Col>
           <Col xs={24} md={24} lg={8}>
             <Form.Item
-              label="Adress"
+              label="Address"
               name="address"
               required
               rules={[{ required: true }]}
             >
-              <Input type="text" placeholder="your first name" />
+              <Input type="text" placeholder="your address" />
             </Form.Item>
           </Col>
         </Row>
@@ -102,24 +136,21 @@ export default function ApplyDoctor() {
               required
               rules={[{ required: true }]}
             >
-              <Input type="text" placeholder="your contact no" />
+              <Input type="text" placeholder="your Fees" />
             </Form.Item>
           </Col>
           <Col xs={24} md={24} lg={8}>
-            <Form.Item
-              label="Timing"
-              name="timing"
-        required
-            >
-           <TimePicker.RangePicker format="HH:mm"/>
+            <Form.Item label="Timing" name="timing" required>
+              <TimePicker.RangePicker format="HH:mm" />
             </Form.Item>
           </Col>
           <Col xs={24} md={24} lg={8}></Col>
           <Col xs={24} md={24} lg={8}>
-             <button className="btn btn-primary form-btn" type="submit">Submit</button>
+            <button className="btn btn-primary form-btn" type="submit">
+              Submit
+            </button>
           </Col>
         </Row>
-      
       </Form>
     </Layout>
   );
